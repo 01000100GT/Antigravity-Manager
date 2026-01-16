5c31451e4354eedb127a96580bf482be06e6e39f
xycxl <>
2026-01-16 20:01:48 +0800

    style: make ProxyMonitor fill entire available space
    - Remove outer padding and border
    - Let component expand to full height/width

--- src/pages/Monitor.tsx
+++ src/pages/Monitor.tsx
@@ -3,11 +3,8 @@ import { ProxyMonitor } from '../components/proxy/ProxyMonitor';
 
 const Monitor: React.FC = () => {
     return (
-        <div className="h-full w-full flex flex-col bg-gray-50 dark:bg-base-200">
-            {/* Main Content (Full Screen Monitor) */}
-            <div className="flex-1 p-4 overflow-hidden">
-                <ProxyMonitor className="h-full border border-gray-200 dark:border-base-300 shadow-md" />
-            </div>
+        <div className="h-full w-full overflow-hidden">
+            <ProxyMonitor className="h-full" />
         </div>
     );
 };
